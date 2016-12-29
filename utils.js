4df382674cae61572e3d1b116d4a73be39b4873b
Jon Kolman <jonathankolman@gmail.com>
2016-12-29 13:18:33 -0500

    utils

--- utils.js
+++ utils.js
@@ -11,6 +11,17 @@ export const listify = (obj) => {
   }
 }
 
+export const eliminateDuplicates = (array) => {
+  let obj = {}
+  let out = []
+  for(let i=0; i<array.length; i++) {
+    obj[array[i]] = 0
+  }
+  for (let x in obj) {
+    out.push(x)
+  }
+  return out
+}
 export const statusText = {
   0: 'Not Started',
   1: 'Running',
