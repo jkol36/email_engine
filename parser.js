da855b5c3a1cf080558719a290fde0ad10698951
Jon Kolman <jonathankolman@gmail.com>
2017-05-14 19:40:48 -0400

    commented out bio

--- parser.js
+++ parser.js
@@ -23,7 +23,7 @@ export const parseProfile = data => {
       returnData.name = json.entry_data.ProfilePage[0].user.full_name
       returnData.username = username
       returnData.lastPicCode = pics.length > 0 ? pics[0].code: null
-      returnData.bio = json.entry_data.ProfilePage[0].user.biography
+      //returnData.bio = json.entry_data.ProfilePage[0].user.biography
       returnData.url = 'https://www.instagram.com/'+username
     }
     catch(err) {
