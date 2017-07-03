2e469c556d40708cd8ca9b259fa854f8e54cdc22
Jon Kolman <jonathankolman@gmail.com>
2017-07-03 16:59:20 -0400

    josh's querieis

--- app.js
+++ app.js
@@ -23,8 +23,7 @@ import {
   getFollowers,
   getInfluencerProfile,
   getSuggesstions,
-  getPicDetails,
-  getShit
+  getPicDetails
 } from './helpers'
 import {
   parseProfile
@@ -317,7 +316,133 @@ const dispatchQueries = () => {
       type: 'Influencer', 
       id: ID(),
       batchId: ID(),
-      payload: 'igor_sevriugin'
+      payload: 'toneitupnutrition'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: 'vega_team'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: 'green_blender'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: 'alohamoment'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: 'yoursuperfoods'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: 'humnutrition'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: 'gardenoflife'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: 'sunwarriortribe'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: 'toneitupnutrition'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: '22daysnutrition'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: 'toneitupnutrition'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: 'getyuve'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: 'organifi'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: 'nutiva'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: 'philosophielove'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: 'amazinggrass'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: 'drinkorgain'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: 'utopicnutrition'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: 'kachavatribe'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: 'sproutliving'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: 'plantfusion'
+    },
+    {
+      type: 'Influencer', 
+      id: ID(),
+      batchId: ID(),
+      payload: 'rawfusionprotein'
     }
   ]
   return Promise.all(Promise.map(queries, query => dispatch(createQuery(query))))
--- config.js
+++ config.js
@@ -26,7 +26,7 @@ export const headers = {
   'referer': 'https://www.instagram.com/explore/tags/poker/',
   'authority': 'www.instagram.com'
 };
-export const currentVersion = 'igor_sevriugin'
+export const currentVersion = 'josh_fetchmate'
 export const firebaseRef = firebase.database().ref(`igbot/${currentVersion}`)
 export const uniqueEmailRef = firebase.database().ref(`igbot/${currentVersion}/uniqueEmails`)
 export const uniqueEmailCount = firebase.database().ref(`igbot/${currentVersion}/uniqueEmailCount`)
